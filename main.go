2efceced9a620bd6acf5bfa25ae5fbbaa954fbda
victor <victorsleiman25@gmail.com>
2021-09-10 19:08:21 -0300

    Creating API and config method  http

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+
+
+
+	http.HandleFunc("/greet", greet)
+
+	log.Fatal(http.ListenAndServe("localhost:8000",nil))
+
+
+}
+
+func greet(w http.ResponseWriter, r *http.Request){
+
+	fmt.Fprint(w,"hello")
+
+
+
+}
