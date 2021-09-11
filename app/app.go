9eabeaffedc86f9d289d489d5cf76c61876bdb46
victor <victorsleiman25@gmail.com>
2021-09-10 22:57:57 -0300

    create a endpoint (post) createCustomer

--- app/app.go
+++ app/app.go
@@ -16,6 +16,9 @@ func Start(){
 	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}",getCustomerById).Methods(http.MethodGet)
 
+	router.HandleFunc("/customers",createCustomer).Methods(http.MethodPost)
+
+
 	log.Fatal(http.ListenAndServe("localhost:8000",router))
 }
 
@@ -23,5 +26,12 @@ func getCustomerById(w http.ResponseWriter,r*http.Request){
 
 	vars:=mux.Vars(r)
 	fmt.Fprint(w,vars["customer_id"])
+
+}
+
+
+func createCustomer(w http.ResponseWriter,r*http.Request){
+
+   fmt.Fprint(w,"postrecieveeee")
 
 }
