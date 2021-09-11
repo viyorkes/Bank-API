582e0b64ee5cbbd49d429f9ab47f641697827d82
victor <victorsleiman25@gmail.com>
2021-09-10 22:15:12 -0300

    add mux in project

--- app/app.go
+++ app/app.go
@@ -3,15 +3,16 @@ package app
 import (
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func Start(){
 
 
-	mux :=http.NewServeMux()
-
+	//mux :=http.NewServeMux()
+mux := mux.NewRouter()
 	mux.HandleFunc("/greet", greet)
 	mux.HandleFunc("/customers", getAllCustomers)
 
-	log.Fatal(http.ListenAndServe("localhost:8000",mux))
+	log.Fatal(http.ListenAndServe("localhost:8000",mux	))
 }
